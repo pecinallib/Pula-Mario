f451ff9c5aea3d6c1ddbcbe48680a8ae9046c9bc
Matheus Pecinalli <pecinallipoker@gmail.com>
2023-01-12 00:07:26 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let mario = document.querySelector('.mario');
+let pipe = document.querySelector('.pipe');
+
+let jump = () => {
+  mario.classList.add('jump');
+
+  setTimeout(() => {
+    mario.classList.remove('jump');
+  }, 600);
+};
+
+let loop = setInterval(() => {
+  let pipePosition = pipe.offsetLeft;
+  let marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 105) {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = '/img/game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '50px';
+
+    clearInterval(loop);
+  }
+}, 10);
+
+document.addEventListener('keydown', jump);
